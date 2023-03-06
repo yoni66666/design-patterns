9c837b0cc9fa9498971e1c9e56575f2c16cf3d8f
yoni6666 <yonishapiro2@gmail.com>
2023-03-06 09:47:07 +0200

    factory change

--- factoryTree/FactoryTree.java
+++ factoryTree/FactoryTree.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class FactoryTree {
     Factory <String, FileComponent,String> factory;
     private final FileComponent folder;
